cd8c976ceaa27f938a610a20c0a47bcb6413d608
Matthias Kutscheid <eifelschaf@gmail.com>
2017-09-08 19:26:37 +0200

    create an about activity with databinding

--- app/src/main/java/de/kleinelamas/svbrockscheid/AboutActivity.kt
+++ app/src/main/java/de/kleinelamas/svbrockscheid/AboutActivity.kt
@@ -1,12 +1,28 @@
 package de.kleinelamas.svbrockscheid
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import de.kleinelamas.svbrockscheid.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
 
+    private val icons8Link = "https://icons8.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        val binding = DataBindingUtil.setContentView<ActivityAboutBinding>(this, R.layout.activity_about)
+        binding.handler = this
+    }
+
+
+    fun onIcons8LinkClicked(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(icons8Link)
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        startActivity(intent)
     }
 }
